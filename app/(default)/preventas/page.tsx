f8a788e4e0121647ebcd257d0fb44759667e6bfd
isumalatov <isumalatov@gmail.com>
2024-04-15 21:21:10 +0200

    sidebar cleaned and route changed

--- app/(default)/preventas/page.tsx
+++ app/(default)/preventas/page.tsx
@@ -1,11 +1,11 @@
 export const metadata = {
-  title: 'Dashboard - Mosaic',
-  description: 'Page description',
+  title: 'Preventas',
+  description: 'Pagina de preventas',
 }
 
 import WelcomeBanner from './welcome-banner'
 
-export default function Dashboard() {
+export default function Preventas() {
   return (
     <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-[96rem] mx-auto">
       <WelcomeBanner />
