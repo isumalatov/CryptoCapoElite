a031d0490610710bd1a6b297d55dcebcca9e6ba6
isumalatov <isumalatov@gmail.com>
2024-04-15 21:36:25 +0200

    small things

--- app/(default)/dashboard/page.tsx
+++ app/(default)/dashboard/page.tsx
@@ -5,7 +5,7 @@ export const metadata = {
 
 import WelcomeBanner from './welcome-banner'
 
-export default function Preventas() {
+export default function Dashboard() {
   return (
     <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-[96rem] mx-auto">
       <WelcomeBanner />
