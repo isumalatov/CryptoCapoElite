dfa27033462a15721da0edddbc99faf3ca08a2b3
isumalatov <isumalatov@gmail.com>
2024-04-21 02:58:04 +0200

    more fixing

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,25 +1,103 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import dbConnect from "@/app/lib/dbConnect";
+import User from "@/models/User";
 
-export async function signup(formData: FormData) {
-  const rawFormData = {
-    email: formData.get("email"),
-    name: formData.get("name"),
-    password: formData.get("password"),
-    allowemail: formData.get("allowemail"),
-  };
-  console.log("signup", rawFormData);
-  redirect("/");
+export async function signup(prevState: any, formData: FormData) {
+  try {
+    await dbConnect();
+
+    const email = formData.get("email") as string;
+    const name = formData.get("name") as string;
+    const password = formData.get("password") as string;
+    const repeatpassword = formData.get("repeatpassword") as string;
+    const allowemail = formData.get("allowemail") === "on";
+
+    if (
+      !email ||
+      email.trim() === "" ||
+      !name ||
+      name.trim() === "" ||
+      !password ||
+      password.trim() === "" ||
+      !repeatpassword ||
+      repeatpassword.trim() === ""
+    ) {
+      return { success: false, message: "Rellena todos los campos" };
+    }
+
+    // Comprobar si la contraseña tiene entre 8 y 20 caracteres
+    if (password.length < 8 || password.length > 20) {
+      return {
+        success: false,
+        message: "La contraseña debe tener entre 8 y 20 caracteres",
+      };
+    }
+
+    // Comprobar si el nombre no supera los 20 caracteres
+    if (name.length > 20) {
+      return {
+        success: false,
+        message: "El nombre no puede superar los 20 caracteres",
+      };
+    }
+
+    if (password !== repeatpassword) {
+      return { success: false, message: "Las contraseñas no coinciden" };
+    }
+
+    // comprobar si el email ya está registrado
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      return { success: false, message: "El email ya está registrado" };
+    }
+
+    const user = new User({
+      email: email,
+      name: name,
+      password: password,
+      allowemail: allowemail,
+      discord: null,
+      telegram: null,
+    });
+    await user.save();
+    return { success: true, message: "Cuenta creada con éxito" };
+  } catch (error) {
+    // Handle the error here
+    console.error(error);
+    return { success: false, message: "Error al crear la cuenta" };
+  }
 }
 
-export async function signin(formData: FormData) {
-  const rawFormData = {
-    email: formData.get("email"),
-    name: formData.get("password"),
-  };
-  console.log("signin", rawFormData);
-  redirect("/");
+export async function signin(prevState: any, formData: FormData) {
+  try {
+    await dbConnect();
+
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    if (!email || email.trim() === "" || !password || password.trim() === "") {
+      return { success: false, message: "Rellena todos los campos" };
+    }
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return { success: false, message: "Email o contraseña incorrectos" };
+    }
+
+    if(password !== user.password) {
+      return { success: false, message: "Email o contraseña incorrectos" };
+    }
+
+    return { success: true, message: "Sesión iniciada con éxito" };
+    
+  } catch (error) {
+    // Handle the error here
+    console.error(error);
+    return { success: false, message: "Error al iniciar sesión" };
+  }
 }
 
 export async function logout() {
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,11 +1,12 @@
 export interface SignInFormData {
   email: string;
   password: string;
-} 
+}
 
 export interface SignUpFormData {
   email: string;
   name: string;
   password: string;
+  repeatpassword: string;
   allowemail: boolean;
 }
