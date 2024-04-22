fcd22a42f20192247e85a13515b889752cf5f26a
isumalatov <isumalatov@gmail.com>
2024-04-22 17:08:09 +0200

    logo deleted

--- components/dropdown-profile.tsx
+++ components/dropdown-profile.tsx
@@ -22,13 +22,6 @@ export default function DropdownProfile({
   return (
     <Menu as="div" className="relative inline-flex">
       <Menu.Button className="inline-flex justify-center items-center group">
-        <Image
-          className="w-8 h-8 rounded-full"
-          src={UserAvatar}
-          width={32}
-          height={32}
-          alt="User"
-        />
         <div className="flex items-center truncate">
           <span className="truncate ml-2 text-sm font-medium dark:text-slate-300 group-hover:text-slate-800 dark:group-hover:text-slate-200">
             CryptoCapoElite
