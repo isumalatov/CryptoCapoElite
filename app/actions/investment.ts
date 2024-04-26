3817b45fa718b65b321a467f5ae134bdcda22c53
isumalatov <isumalatov@gmail.com>
2024-04-27 01:19:17 +0200

    investments endpoint

--- app/actions/investment.ts
+++ app/actions/investment.ts
@@ -63,3 +63,48 @@ export async function fetchuserinvestments() {
     return { success: false, message: "Error al cargar inversiones" };
   }
 }
+
+export async function createinvestment(investmentData: InvestmentDataTable) {
+  try {
+    await dbConnect();
+    const investment = new Investment(investmentData);
+    await investment.save();
+    return { success: true, message: "Inversión creada" };
+  } catch (err) {
+    console.log(err);
+    return { success: false, message: "Error al crear inversión" };
+  }
+}
+
+export async function deleteinvestment(id: string) {
+  try {
+    await dbConnect();
+    const investment = await Investment.findById(id);
+    if (!investment) {
+      return { success: false, message: "Inversión no encontrada" };
+    }
+    await Investment.deleteOne({ _id: id });
+    return { success: true, message: "Inversión eliminada" };
+  } catch (err) {
+    console.log(err);
+    return { success: false, message: "Error al eliminar inversión" };
+  }
+}
+
+export async function updateinvestment(
+  id: string,
+  investmentData: InvestmentDataTable
+) {
+  try {
+    await dbConnect();
+    const investment = await Investment.findById(id);
+    if (!investment) {
+      return { success: false, message: "Inversión no encontrada" };
+    }
+    await Investment.updateOne({ _id: id }, investmentData);
+    return { success: true, message: "Inversión actualizada" };
+  } catch (err) {
+    console.log(err);
+    return { success: false, message: "Error al actualizar inversión" };
+  }
+}
